c87820ea289f9dabd5a71df1da06da2ed29bb81a
Drew Pitchford <dpitchford@me.com>
2018-06-28 16:16:45 -0500

    Updated framework

---
+++ DPKitchenSink.framework/Headers/DPKitchenSink.h
@@ -0,0 +1,18 @@
+//
+//  DPKitchenSink.h
+//  DPKitchenSink
+//
+//  Created by Drew Pitchford on 6/27/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DPKitchenSink.
+FOUNDATION_EXPORT double DPKitchenSinkVersionNumber;
+
+//! Project version string for DPKitchenSink.
+FOUNDATION_EXPORT const unsigned char DPKitchenSinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DPKitchenSink/PublicHeader.h>
+
+
